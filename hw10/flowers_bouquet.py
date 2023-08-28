844908e0e7c9f91c4ee3afde59136a93db014573
gonchar-viktor <vita-sc2@mail.ru>
2023-08-29 00:17:58 +0300

    fix hw 10

--- hw10/flowers_bouquet.py
+++ hw10/flowers_bouquet.py
@@ -28,18 +28,31 @@ class Flower:
 
 
 class Chamomile(Flower):  # Ромашка
-    def __init__(self):
+    def __init__(self, core="core"):
+        self.core = core
         super().__init__("Chamomile", 3, 120)
+
+    def chamomile_core(self):
+        return f"Chamomile has a pronounced and large {self.core}."
 
 
 class Rose(Flower):  # Роза
-    def __init__(self):
+    def __init__(self, thorns="thorns"):
+        self.thorns = thorns
         super().__init__("Rose", 5, 96)
+
+    def rose_thorns(self):
+        return f"A rose differs from other flowers in that it " \
+               f"has{self.thorns}."
 
 
 class Tulip(Flower):  # Тюльпан
-    def __init__(self):
+    def __init__(self, pollen="pollen"):
+        self.pollen = pollen
         super().__init__("Tulip", 4, 72)
+
+    def tulip_pollen(self):
+        return f"There is a lot of {self.pollen} in a tulip."
 
 
 print()
@@ -47,35 +60,81 @@ cham = Chamomile()
 ros = Rose()
 tul = Tulip()
 
+
 # Сбор букета с определением стоимости
-bouquet = [cham, ros, ros, tul, tul, tul]
-the_cost_of_the_bouquet = sum(i.cost() for i in bouquet)
-print("The cost of the bouquet:", the_cost_of_the_bouquet, "Br")
+def bouquet_flower():
+    global bouquet
+    bouquet = [cham, ros, ros, tul, tul, tul]
+    cost_accessories = 3  # аксессуар ленточка
+    the_cost_of_the_bouquet = sum(i.cost() for i in bouquet)
+    print("The cost of the bouquet:", the_cost_of_the_bouquet +
+          cost_accessories, "Br")
+    print(f"The cost of the ribbon for flowers: {cost_accessories} Br")
+
+
+bouquet_flower()
+
 
 # Определение времени увядания букета
+def time_flower():
+    time = sum(i.time() for i in bouquet) / len(bouquet)
+    print("Average wilting time:", time, "hours")
 
-time = sum(i.time() for i in bouquet) / len(bouquet)
-print("Average wilting time:", time, "hours")
+
+time_flower()
+
 
 # Сортировка цветов по стоимости
+def sort():
+    sorted_bouquet = sorted(bouquet, key=lambda i: i.cost(), reverse=True)
+    print("Sorting colors by cost in descending order", [i.name_flower for i in
+                                                         sorted_bouquet], "\n")
 
-sorted_bouquet = sorted(bouquet, key=lambda i: i.cost(), reverse=True)
-print("Sorting colors by cost in descending order", [i.name_flower for i in
-                                                     sorted_bouquet])
+
+sort()
+
 
 # Проверка, есть ли цветок в букете
+def check_flowers(flower):
+    """
+    Checks if there is a flower in the bouquet
+    """
+    check = any(i.name() == flower for i in bouquet)
+    print(f"Flower\" {flower}\" there is in the bouquet: {check}")
 
-flower_name1 = "Chamomile"
-flower_name2 = "Windows 10"
-flower_name3 = "Tulip"
+
+check_flowers("Chamomile")
+check_flowers("Windows 10")
+check_flowers("Tulip")
 print()
-check = any(i.name() == flower_name1 for i in bouquet)
-print(f"Flower \"{flower_name1}\" there is in the bouquet: {check}")
-check = any(i.name() == flower_name2 for i in bouquet)
-print(f"Flower \"{flower_name2}\" there is in the bouquet: {check}")
-check = any(i.name() == flower_name3 for i in bouquet)
-print(f"Flower \"{flower_name3}\" there is in the bouquet: {check}")
+
+
+def number_flowers(flower, variable):
+    """
+    Learns the number of specific flowers in a bouquet
+    """
+    print(f"{flower} in a bouquet: {bouquet.count(variable)} pieces.")
+
+
+number_flowers("Chamomiles", cham)
+number_flowers("Roses", ros)
+number_flowers("Tulips", tul)
 print()
-print("Roses in a bouquet:", bouquet.count(ros), "pieces")
-print("Chamomiles in a bouquet:", bouquet.count(tul), "pieces")
-print("Tulips in a bouquet:", bouquet.count(cham), "pieces")
+
+
+def features():
+    print("\tFeatures of flowers:")
+    print(cham.chamomile_core())
+    print(ros.rose_thorns())
+    print(tul.tulip_pollen())
+
+
+features()
+
+# добавил особенности в класс к каждому цветку
+# счётчик цветов обернул в функцию
+# проверку на то, есть ли цветок в букете обернул в функцию
+# сортировку цветов по стоимости обернул в функцию
+# определение времени увядания букета обернул в функцию
+# сбор букета с определением стоимости обернул в функцию
+# добавил аксессуар "ленточка"
